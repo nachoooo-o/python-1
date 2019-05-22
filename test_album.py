1f82515e164411e9d304f0bc346a01a1907a8767
nachoooo-o <50930586+nachoooo-o@users.noreply.github.com>
2019-05-22 18:39:30 -0300

    Add files via upload

---
+++ test_album.py
@@ -0,0 +1,56 @@
+import random
+
+# Variables
+cantidadDeMuestras = 4
+albumCompleto = False
+sobresAbiertos = 0
+album = []
+sobre = []
+muestra = []
+
+# Lleno el album todo vacio
+for a in range(0, 99):
+    album.append(False)
+
+#abrir sobre
+def abrirSobre():
+    for s in range(0,5):
+        sobre.append(random.randint(0, 98))
+    return sobre
+
+#ingresar sobre en album
+def pegarFigAlbum():
+    sobre = abrirSobre()
+    global sobresAbiertos
+    sobresAbiertos += 1
+    for numeroFigurita in sobre:
+        album[numeroFigurita] = True
+
+def checkearAlbumCompleto():
+    global albumCompleto
+    if False in album:
+       albumCompleto = False
+    else:
+        albumCompleto = True
+        albumCompleto += 1
+
+while albumCompleto == False:
+    pegarFigAlbum()
+    checkearAlbumCompleto()
+
+if albumCompleto == True:
+    print(sobresAbiertos)
+
+for m in range(0, cantidadDeMuestras):
+    muestra.append(sobresAbiertos)
+
+print (muestra)
+
+
+
+
+#    pegarFigAlbum()
+#   checkearAlbumCompletado()
+
+
+
